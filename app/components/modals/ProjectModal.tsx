f0ceb3d962d1f32d87dff5d4bf7a690fcfa14ce8
郭秦杰 <11277252+j620@user.noreply.gitee.com>
2024-03-26 22:31:32 +0800

    feat: 项目modal初始化内容

--- app/components/modals/ProjectModal.tsx
+++ app/components/modals/ProjectModal.tsx
@@ -1,11 +1,31 @@
 "use client"
 import Modal from "@/app/components/modals/Modal";
 import useProjectModal from "@/app/hooks/useProjectModal";
+import Input from "@/app/components/Input";
+import {useForm} from "react-hook-form";
 
 const ProjectModal: React.FC = () => {
     const projectModal = useProjectModal()
+    const {
+        register,
+        handleSubmit,
+        formState: {errors}
+    } = useForm()
+    const bodyContent = (
+        <form className={"space-y-3"}>
+            <Input id={"title"} label={"标题"} register={register} errors={errors}/>
+            <Input id={"time"} label={"时间"} register={register} errors={errors}/>
+            <Input id={"job"} label={"职责"} register={register} errors={errors}/>
+            <Input id={"stack"} label={"技术栈"} register={register} errors={errors}/>
+            <Input id={"describe"} label={"描述"} register={register} errors={errors}/>
+            <Input id={"highlight"} label={"亮点"} register={register} errors={errors}/>
+        </form>
+    )
     return (
-        <Modal isOpen={projectModal.isOpen} onClose={projectModal.onClose} onSubmit={projectModal.onClose}
+        <Modal isOpen={projectModal.isOpen}
+               onClose={projectModal.onClose}
+               onSubmit={projectModal.onClose}
+               body={bodyContent}
                actionLabel={"添加"}/>
     )
 }
